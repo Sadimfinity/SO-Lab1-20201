5e0d1dacbb1e795706cf7c41eed4cd3aad00e34a
sadimfinity <santi-skate@hotmail.com>
2020-09-24 00:08:58 -0500

    wget done

---
+++ enunciado/wgrep/wgrep.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    FILE *fp = fopen(argv[2], "r");
+    if (fp == NULL)
+    {
+        printf("wgrep: cannot open file\n");
+        exit(1);
+    }
+    size_t size = 10;
+    char *line = malloc(sizeof(char)*size);
+    line = (char *)argv[1];
+
+    getline(&line, &size, fp);
+    printf("%s", line);
+    return 0;
+}
