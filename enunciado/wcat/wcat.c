de4bb87c5ef9183f988c1edfc79d65c7ab9606ad
sadimfinity <santi-skate@hotmail.com>
2020-09-27 22:30:19 -0500

    Wcat and wgrep done

--- enunciado/wcat/wcat.c
+++ enunciado/wcat/wcat.c
@@ -6,27 +6,19 @@ int main(int argc, char const *argv[])
     int i;
     for (i = 1; i < argc; i++){
         FILE *fp = fopen(argv[i], "r");
-        // {
-        //     printf("wcat: cannot open file\n");
-        //     exit(1);
-        // }
-        // char ch;
-        // while((ch = getc(fp)) != EOF){
-        //     printf("%c", ch);
-
-        // }
         if (fp == NULL)
         {
-            printf("cannot open file\n");
+            printf("wcat: cannot open file\n");
             exit(1);
         }
-        char *s;
-        while((s = fgets(s, sizeof(*fp), fp)) != EOF){
+        int val;
+        char *s = malloc(sizeof(char));
+        while((val = getc(fp)) != EOF){
+            *s = val;
             if (s == NULL)
                 break;
             else
                 printf("%s", s);
-            free(s);
         }
     }
     return 0;
