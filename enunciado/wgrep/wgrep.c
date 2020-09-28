1d7bbd6b296f0cfb8a815c169fe83c4e020e6e8b
sadimfinity <santi-skate@hotmail.com>
2020-09-27 22:28:43 -0500

    Wcat and wgrep done

--- enunciado/wgrep/wgrep.c
+++ enunciado/wgrep/wgrep.c
@@ -1,19 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    FILE *fp = fopen(argv[2], "r");
-    if (fp == NULL)
+    if (argc == 1)
     {
-        printf("wgrep: cannot open file\n");
-        exit(1);
+        printf("wgrep: searchterm [file ...]\n");
+        return 1;
     }
-    size_t size = 10;
-    char *line = malloc(sizeof(char)*size);
-    line = (char *)argv[1];
 
-    size_t result = getline(&line, &size, fp);
-    printf("%s", result);
+    char *hilera = argv[1];
+    if (argc == 2)
+    {
+        char limteMax[100];
+        while ( fgets(limteMax, 100, stdin) ){
+            if (strstr(limteMax, hilera) != NULL)
+            {
+                printf("%s", limteMax);
+            }
+        }
+    }
+    if (argc > 2)
+    {
+        for (int i = 2; i < argc; i++)
+        {
+            char *hilera2 = NULL;
+            size_t len = 0;
+            ssize_t read;
+            FILE *fp = fopen(argv[i], "r");
+            if (fp == NULL)
+            {
+                printf("wgrep: cannot open file\n");
+                return 1;
+                
+            }
+            while ((read = getline(&hilera2, &len, fp)) != -1)
+            {
+                if (strstr(hilera2, hilera) != NULL)
+                {
+                    printf("%s", hilera2);
+                }
+            }
+            fclose(fp);
+        }
+    }
+
     return 0;
 }
